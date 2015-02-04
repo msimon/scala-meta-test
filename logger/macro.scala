35fe7d9b0c8d1ade2bcc4dd506c404cb69dab6a1
Simon Marc <marc.simon42@gmail.com>
2015-02-04 11:34:58 +0800

    first implem of logEntries logger

--- logger/macro.scala
+++ logger/macro.scala
@@ -6,7 +6,57 @@ object LoggerMacro {
   def logImpl(c: Context)(x: c.Tree) : c.Tree = {
     import c.universe._
 
-    println(showRaw(x));
+    val argDeliStart = "|>"
+    val argDeliEnd = "<|"
+    def treeToString(t: c.Tree, a: String = "") : String = {
+      t match {
+        case Literal(Constant(b: String)) => a + b
+
+        case q"""scala.StringContext.apply(..$lcons).s(..$lparams)""" => // use of s""
+
+          def concat(lcons : List[Tree], lpar: List[Tree], acc: String= "") : String = {
+            (lcons.headOption, lpar.headOption) match {
+              // case (Some(Literal(Constant(cons))), Some(Ident(TermName(par)))) => concat(lcons.tail, lpar.tail, acc + cons + "$>" + par)
+              case (Some(Literal(Constant(cons))), Some(par)) => concat(lcons.tail, lpar.tail, acc + cons + argDeliStart + par + argDeliEnd)
+              case (Some(Literal(Constant(cons))), None) => acc + cons
+              case _ => ""
+            }
+          }
+          concat(lcons, lparams)
+
+        case Apply(Select(selectBody, TermName("$plus")), (applyParam::Nil)) => // use of '+'
+          val b = treeToString(selectBody, a);
+          treeToString(applyParam, b);
+
+        case Apply(Select(_, TermName(name)), _) => // function application
+          val b = argDeliStart + name + argDeliEnd
+          a + b
+        case Select(_, TermName(name)) => // value
+          val b = argDeliStart + name + argDeliEnd
+          a + b
+      }
+    }
+
+    def pseudoChecker(s: String) = {
+      val m = java.util.regex.Pattern.compile(
+        """((\w+)?\=?)\s*""" + argDeliStart.replace("|", "\\|") + """(.*?)""" + argDeliEnd.replace("|", "\\|")
+      )
+      val res = m.matcher(s);
+
+      while (res.find) {
+        val key = res.group(1);
+        val value = res.group(3);
+        println(s"""key: "$key" value: "$value"""");
+        key.lastOption match {
+          case (Some(ch)) if (ch == '=') => ()
+          case _ => c.error(c.enclosingPosition, "Ill Structured Log\n")
+        }
+        println("> Pass\n");
+      }
+    }
+
+    val strToParse = treeToString(x);
+    pseudoChecker(strToParse);
 
     q"""
        println("Loggin: " + $x)
--- logger/test.scala
+++ logger/test.scala
@@ -1,96 +1,19 @@
 object Test extends App {
+  object T2 {
+    def f(v: String) = "`v`";
+  }
   val avalue = "AValue"
+  val bvalue = "BValue"
   def f(v: String) = "`v`";
 
-
   LoggerMacro.log("My first log");
+  LoggerMacro.log("My second log=" + avalue);
 
-  // LoggerMacro.log("My second log" + avalue + " test" + avalue);
-
-  // Apply(
-  //   Select(
-  //     Apply(
-  //       Select(
-  //         Apply(
-  //           Select(
-  //             Literal(Constant("My second log")), TermName("$plus")
-  //           ),
-  //           List(Select(This(TypeName("Test")), TermName("avalue")))
-  //         ),
-  //         TermName("$plus")
-  //       ),
-  //       List(Literal(Constant(" test")))
-  //     ),
-  //     TermName("$plus")
-  //   ),
-  //   List(Select(This(TypeName("Test")), TermName("avalue")))
-  // )
+  LoggerMacro.log("My second log=" + avalue + " test" + avalue);
 
   // LoggerMacro.log("My second log" + avalue + " test" + f(avalue));
 
-  // Apply(
-  //   Select(
-  //     Apply(
-  //       Select(
-  //         Apply(
-  //           Select(
-  //             Literal(Constant("My second log")), TermName("$plus")
-  //           ),
-  //           List(Select(This(TypeName("Test")), TermName("avalue")))
-  //         ),
-  //         TermName("$plus")
-  //       ),
-  //       List(Literal(Constant(" test")))
-  //     ),
-  //     TermName("$plus")
-  //   ),
-  //   List(
-  //     Apply(
-  //       Select(
-  //         This(TypeName("Test")), TermName("f")
-  //       ),
-  //       List(
-  //         Select(
-  //           This(TypeName("Test")), TermName("avalue")
-  //         )
-  //       )
-  //     )
-  //   )
-  // )
+  // LoggerMacro.log(avalue + "My second log test=" + T2.f(avalue));
 
-  LoggerMacro.log(avalue + "My second log test" + f(avalue));
-
-  // Apply(
-  //   Select(
-  //     Apply(
-  //       Select(
-  //         Select(
-  //           This(TypeName("Test")), TermName("avalue")
-  //         ),
-  //         TermName("$plus")
-  //       ),
-  //       List(
-  //         Literal(
-  //           Constant("My second log test")
-  //         )
-  //       )
-  //     ),
-  //     TermName("$plus")
-  //   ),
-  //   List(
-  //     Apply(
-  //       Select(
-  //         This(TypeName("Test")),
-  //         TermName("f")
-  //       ),
-  //       List(
-  //         Select(
-  //           This(TypeName("Test")),
-  //           TermName("avalue"))
-  //       )
-  //     )
-  //   )
-  // )
-
-  LoggerMacro.log(s"My second log $avalue");
+  LoggerMacro.log(s"My second log=$avalue, $bvalue");
 }
